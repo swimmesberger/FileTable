a74163fbcff4a1e2b02e430f173bcec307a7037f
Simon Wimmesberger <wimmesberger@gmail.com>
2014-10-05 18:46:59 +0200

    Removed old license header

--- src/main/java/org/fseek/simon/swing/filetable/FileTableModel.java
+++ src/main/java/org/fseek/simon/swing/filetable/FileTableModel.java
@@ -1,19 +1,3 @@
-/*
- * Copyright (C) 2011 Simon Wimmesberger<www.fseek.org>
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
 package org.fseek.simon.swing.filetable;
 
 import java.io.File;
@@ -24,7 +8,10 @@ import org.apache.commons.io.FilenameUtils;
 import org.fseek.simon.swing.filetable.i18n.I18nData;
 import org.fseek.thedeath.os.icons.FileIconWrapper;
 import org.fseek.thedeath.os.util.OSUtil;
-
+/**
+ *
+ * @author Simon Wimmesberger
+ */
 public class FileTableModel extends DefaultTableModel implements Serializable
 {
     public static final int NAME_COLUMN_IDX = 0;
